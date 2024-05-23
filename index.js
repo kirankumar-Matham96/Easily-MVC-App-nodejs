fc58ca75b8124117bc425569b3341456d222fb01
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2024-05-23 20:42:38 +0530

    feat: navbar added

--- index.js
+++ index.js
@@ -8,8 +8,12 @@ const app = express();
 // using json parser
 app.use(express.json());
 
+// applying static path
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.status(200).send({ message: "Welcome to Easily App" });
+  // res.status(200).send({ message: "Welcome to Easily App" });
+  res.status(200).send("/index.html");
 });
 
 app.listen(PORT, () => {
