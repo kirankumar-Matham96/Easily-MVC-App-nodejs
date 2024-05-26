214d211ad3991ec7ef90c5ba2197836a842e69aa
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2024-05-27 03:02:15 +0530

    bug-fix: lastvisit middleware adjusted with time format

--- src/middlewares/lastVisit.middleware.js
+++ src/middlewares/lastVisit.middleware.js
@@ -1,10 +1,36 @@
 import cookieParser from "cookie-parser";
 
+const formatDate = (inputDate = "") => {
+  let date;
+  if (inputDate) {
+    date = new Date(inputDate);
+  } else {
+    date = new Date();
+  }
+
+  const month = date.toLocaleString("default", { month: "long" });
+  const formattedDateTimeString =
+    month +
+    " " +
+    date.getDate() +
+    ", " +
+    date.getFullYear() +
+    " at " +
+    date.toLocaleTimeString();
+  return formattedDateTimeString;
+};
+
 export const lastVisit = (req, res, next) => {
   if (req.cookies.lastVisit) {
-    res.locals.lastVisit = new Date(req.cookies.lastVisit).toISOString();
+    const date = new Date(req.cookies.lastVisit).toISOString();
+    res.locals.lastVisit = date;
+    res.locals.lastVisitFormatted = formatDate(date);
   } else {
-    res.cookie("lastVisit", new Date().toISOString(), {
+    const date = new Date().toISOString();
+    res.cookie("lastVisit", date, {
+      maxAge: 1 * 24 * 60 * 60 * 1000,
+    });
+    res.cookie("lastVisitFormatted", formatDate(date), {
       maxAge: 1 * 24 * 60 * 60 * 1000,
     });
   }
